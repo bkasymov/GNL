49e9d51dd69f64b399bf7d612aad23a670b0c383
dpenney <behaqosimi@gmail.com>
2019-10-07 23:41:08 +0300

    a

---
+++ get_next_lin.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avenonat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/23 20:33:17 by avenonat          #+#    #+#             */
+/*   Updated: 2019/09/24 21:55:20 by avenonat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include "fcntl.h"
+# include "libft/libft.h"
+
+int		ft_new_line(char **s, char **line, int fd, int ret);
+int		get_next_line(const int fd, char **line);
+int		main(int argc, char **argv);
+
+#endif
