1a6e9bc825bcd6f2b8acd5147f5ebf08e1369c05
dpenney <behaqosimi@gmail.com>
2019-10-15 08:34:06 +0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -68,29 +68,64 @@ int			get_next_line(int fd, char **line)
 	static char	*rem[OPEN_MAX];
 	int			rsr;
 
+	rsr = 0;
 	if (fd < 0 || line == NULL || fd > OPEN_MAX || \
 								BUFF_SIZE <= 0)
 		return (-1);
 	return (give_line(fd, line, rem, rsr));
 }
 
-int		main(void)
+
+
+
+
+
+
+
+
+
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*
+**  1 line with 8 chars with Line Feed
+*/
+
+int				main(void)
 {
-	int		fd;
-	int		fd2;
-	char	*line;
-	
-	fd = open("text.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	ft_putchar('\n');
-	fd = open("text2.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	ft_putchar('\n');
-	fd = open("text.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	ft_putchar('\n');
+	char		*line;
+	int			fd;
+	int			ret;
+	int			count_lines;
+	char		*filename;
+	int			errors;
+
+	filename = "gnl1_1.txt";
+	fd = open(filename, O_RDONLY);
+	if (fd > 2)
+	{
+		count_lines = 0;
+		errors = 0;
+		line = NULL;
+		while ((ret = get_next_line(fd, &line)) > 0)
+		{
+			if (count_lines == 0 && strcmp(line, "1234567") != 0)
+				errors++;
+			count_lines++;
+			if (count_lines > 50)
+				break;
+		}
+		close(fd);
+		if (count_lines != 1)
+			printf("-> must have returned '1' once instead of %d time(s)\n", count_lines);
+		if (errors > 0)
+			printf("-> must have read \"1234567\" instead of \"%s\"\n", line);
+		if (count_lines == 1 && errors == 0)
+			printf("OK\n");
+	}
+	else
+		printf("An error occured while opening file %s\n", filename);
 	return (0);
 }
